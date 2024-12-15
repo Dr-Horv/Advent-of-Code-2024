484fbdd64da0fc22dc5c8a3c0b557e042423e9e3
Johan Horvius <drhorv@gmail.com>
2024-12-15 19:49:31 +0100

    Solution for day 15 part2

--- src/main/kotlin/day15/Day15.kt
+++ src/main/kotlin/day15/Day15.kt
@@ -5,28 +5,51 @@ import se.horv.util.Direction
 import se.horv.util.Solver
 
 enum class Tile {
-    BOX,
     WALL,
     EMPTY
+}
+
+data class Box(val id: Int, var coordinates: Set<Coordinate>) {
+    companion object {
+        var idCounter = 0
+    }
+    constructor(coordinates: Set<Coordinate>) : this(idCounter++, coordinates)
+
+    fun move(dir: Direction): Box = Box(id, coordinates.map { it + dir }.toSet())
 }
 
 class Day15: Solver {
     override fun solve(lines: List<String>, partTwo: Boolean): String {
         var robot = Coordinate(-1,-1)
         val map = mutableMapOf<Coordinate, Tile>()
+        val boxes = mutableSetOf<Box>()
         val sequence = mutableListOf<Direction>()
         var y = 0
         for (line in lines) {
             if(line.startsWith("#")) {
-                line.forEachIndexed { x, c ->
+                var x = 0
+                line.forEach { c ->
                     when (c) {
-                        '#' -> map[Coordinate(x, y)] = Tile.WALL
-                        'O' -> map[Coordinate(x, y)] = Tile.BOX
+                        '#' -> addTile(x,y, Tile.WALL, map, partTwo)
+                        'O' -> {
+                            boxes.add(
+                                Box(
+                                    if(!partTwo) mutableSetOf(Coordinate(x,y))
+                                    else mutableSetOf(Coordinate(x,y), Coordinate(x+1,y))
+                                )
+                            )
+                            addTile(x, y, Tile.EMPTY, map, partTwo)
+                        }
                         '@' -> {
                             robot = Coordinate(x, y)
-                            map[Coordinate(x, y)] = Tile.EMPTY
+                            addTile(x, y, Tile.EMPTY, map, partTwo)
                         }
-                        '.' -> map[Coordinate(x, y)] = Tile.EMPTY
+                        '.' -> addTile(x, y, Tile.EMPTY, map, partTwo)
+                    }
+                    if(!partTwo) {
+                        x++
+                    } else {
+                        x+=2
                     }
                 }
             } else if (line.trim() == "") {
@@ -45,75 +68,90 @@ class Day15: Solver {
             y++
         }
 
+
         for (move in sequence) {
-            robot = performMove(move, robot, map)
+            robot = performMove(move, robot, boxes, map)
         }
 
-        return calculateBoxGPSs(map).toString()
-
+        return calculateBoxGPSs(boxes).toString()
 
     }
 
-    private fun printMap(robot: Coordinate, map: Map<Coordinate, Tile>) {
+    private fun addTile(
+        x: Int,
+        y: Int,
+        tile: Tile,
+        map: MutableMap<Coordinate, Tile>,
+        partTwo: Boolean
+    ) {
+        map[Coordinate(x, y)] = tile
+        if (partTwo) {
+            map[Coordinate(x + 1, y)] = tile
+        }
+    }
+
+
+    private fun performMove(move: Direction, robot: Coordinate, boxes: MutableSet<Box>, map: Map<Coordinate, Tile>): Coordinate {
+        val next = robot + move
+        val tile = map.getValue(next)
+        if(tile == Tile.WALL) {
+            return robot
+        } else {
+            val boxesToRemove = mutableSetOf<Box>()
+            val boxesToAdd = mutableSetOf<Box>()
+            val toMove = boxes.filter { it.coordinates.contains(next) }.toMutableSet()
+            while (toMove.isNotEmpty()) {
+                val n = toMove.first()
+                toMove.remove(n)
+                val nx = n.move(move)
+                if(nx.coordinates.all { map.getValue(it) == Tile.EMPTY }) {
+                    toMove.addAll(
+                        boxes.filter { it.id != nx.id && it.coordinates.any { c -> nx.coordinates.contains(c) } }
+                    )
+                    boxesToAdd.add(nx)
+                    boxesToRemove.add(n)
+                } else {
+                    return robot
+                }
+            }
+            boxes.removeAll(boxesToRemove)
+            boxes.addAll(boxesToAdd)
+            return next
+        }
+    }
+
+    private fun calculateBoxGPSs(boxes: Set<Box>): Int =
+        boxes.map { b -> b.coordinates.minBy { c -> c.x } }.sumOf { it.y * 100 + it.x }
+
+
+    private fun printMap(robot: Coordinate, boxes: Set<Box>, map: Map<Coordinate, Tile>) {
         val height = map.keys.maxOf { it.y }
         val width = map.keys.maxOf { it.x }
         var s = ""
         for (y in 0 .. height) {
-            for (x in 0 .. width) {
+            for (x in 0..width) {
                 val coordinate = Coordinate(x, y)
                 val c = map[coordinate] ?: throw IllegalArgumentException()
-                s += if(coordinate == robot) {
-                    "@"
-                } else {
-                    when (c) {
-                        Tile.BOX -> "O"
-                        Tile.WALL -> "#"
-                        Tile.EMPTY -> "."
+                if(robot == coordinate) {
+                    s += "@"
+                } else if (c == Tile.WALL) {
+                    s += "#"
+                } else if (c === Tile.EMPTY) {
+                    val b = boxes.find { it.coordinates.contains(coordinate) }
+                    s += if (b != null) {
+                        if(b.coordinates.minBy { it.x } != coordinate) {
+                            "]"
+                        } else {
+                            "["
+                        }
+                    } else {
+                        "."
                     }
                 }
-
             }
             s += "\n"
         }
         println(s)
     }
 
-    private fun calculateBoxGPSs(map: Map<Coordinate, Tile>): Int =
-        map.entries.filter { it.value == Tile.BOX }.sumOf { it.key.y * 100 + it.key.x }
-
-    private fun attemptPush(to: Coordinate, from: Coordinate, map: MutableMap<Coordinate, Tile>) {
-        if(map.getValue(from) == Tile.BOX && map.getValue(to) == Tile.EMPTY) {
-            map[to] = Tile.BOX
-            map[from] = Tile.EMPTY
-        }
-    }
-
-    private fun performMove(move: Direction, robot: Coordinate, map: MutableMap<Coordinate, Tile>): Coordinate {
-        var nextBreak = robot
-        var next = robot
-        while (true) {
-            next += move
-            if(map[next] == Tile.WALL || map[next] == Tile.EMPTY) {
-                nextBreak = next
-                break
-            }
-        }
-
-        val moveOpposite = move.opposite()
-        var prev = nextBreak
-        var curr = nextBreak + moveOpposite
-        while (curr != robot) {
-            attemptPush(prev,curr, map)
-            prev = curr
-            curr += moveOpposite
-        }
-
-        val newPos = robot + move
-        return if(map.getValue(newPos) == Tile.EMPTY) {
-            newPos
-        } else {
-            robot
-        }
-
-    }
 }
